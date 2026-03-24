cf3457c4769289b212fc11b83928c4be1851dcde
Shamko Aliaksand <shamkoag@gmail.com>
2026-03-24 15:27:35 +0100

    fix bugs

--- backend/src/utils/jwks.ts
+++ backend/src/utils/jwks.ts
@@ -1,5 +1,6 @@
 import { createRemoteJWKSet } from "jose";
 
+const endpoint = process.env.LOGTO_ENDPOINT?.replace(/\/$/, "");
 export const JWKS = createRemoteJWKSet(
-  new URL(`${process.env.LOGTO_ENDPOINT}/oidc/jwks`)
+  new URL(`${endpoint}/oidc/jwks`)
 );
